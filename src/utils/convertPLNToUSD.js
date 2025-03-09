c784449076ea7407405e9184cce1e95411f65f77
Krzysztof Kalkowski <kkalkowski@pulsonline.pl>
2025-03-09 12:21:20 +0100

    Test convertPLNToUSD

--- src/utils/convertPLNToUSD.js
+++ src/utils/convertPLNToUSD.js
@@ -1,6 +1,26 @@
 export const convertPLNToUSD = (PLN) => {
 
   const PLNtoUSD = PLN / 3.5;
+
+  // Checking imput value
+
+  if (typeof PLN === 'string') {
+    return NaN;
+  }
+
+  if (PLN === undefined) {
+    return NaN;
+  }
+
+  if (typeof PLN !== 'number' && typeof PLN !== 'string') {
+    return 'Error';
+  }
+
+  if (PLN < 0) {
+    return '$0.00';
+  }
+
+  // end of checking imput value
   
   const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
